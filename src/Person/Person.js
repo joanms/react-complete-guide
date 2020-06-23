a5e5f5c1c482096ac82183e147364731b9c868f5
Joan Sweeney <joanms@eircom.net>
2020-06-23 18:03:12 +0100

    Pass method reference between components and add 2-way binding

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const person = (props) => {
+const person = ( props ) => {
     return (
         <div>
-            <p>My name is {props.name} and I'm {props.age} years old!</p>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
 };
