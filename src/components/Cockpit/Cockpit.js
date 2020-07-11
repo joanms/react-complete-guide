3489c64217722c9bc2933b0fbb7c473a41e4397d
Joan Sweeney <joanms@eircom.net>
2020-07-11 16:48:10 +0100

    Control useEffect behaviour

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -6,7 +6,10 @@ const cockpit = (props) => {
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
         // Http request...
-    });
+        setTimeout(() => {
+            alert('Saved data to cloud');
+        }, 1000);
+    }, []);
 
     const assignedClasses = [];
     let btnClass = '';
